e853668602a0c6845c19eebf2453ad6f98eb23af
Trevor von Hake <tvonhake@outlook.com>
2021-04-20 14:12:26 -0600

    opening a vacc from my list shows image now.

--- Pages/Vaccination.js
+++ Pages/Vaccination.js
@@ -1,22 +1,49 @@
 import React, {useState, useEffect} from 'react'
-import { Text, View } from 'react-native';
+
 import { Card, CardItem, Button} from 'react-native-elements'
+
+import { Text, View, Image, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  stretch: {
+    aspectRatio: 1,
+    resizeMode: 'contain'
+  },
+});
 
 const Vaccination = (props) => {
 
   const {route} = props
+  const image = route.params.vaccination.image
 
   return (
-    <Card key={route.params.vaccination.id}>
-      <Card.Title>{route.params.vaccination.vaccine_name}</Card.Title>
-      <Card.Divider/>
-        <Text style={{marginBottom: 10}}>
-          MFG: {route.params.vaccination.manufacturer}
-        </Text>
-        <Text style={{marginBottom: 10}}>
-          Issued by: {route.params.vaccination.issuer_name}
-        </Text>
-    </Card>
+    <View >
+      <Card key={route.params.vaccination.id}>
+        <Card.Title>{route.params.vaccination.vaccine_name}</Card.Title>
+        <Card.Divider/>
+          <Text style={{marginBottom: 10}}>
+            MFG: {route.params.vaccination.manufacturer}
+          </Text>
+          <Text style={{marginBottom: 10}}>
+            Issued by: {route.params.vaccination.issuer_name}
+          </Text>
+      </Card>
+      <Text>{"\n"}</Text>
+      <Card>
+        <Card.Title>Vaccination Card Image</Card.Title>
+        <Card.Divider/>
+          {console.log(route.params.vaccination)}
+        <Image
+          style={styles.stretch}
+          source={{
+            uri: image,
+          }}
+        />
+      </Card>
+      </View>
   )
 }
 
